f5a5c9600c0f97f5c67760d6b43743568bf1f1df
Gabriel Tiburcio <v-gtiburcio@mparticle.com>
2024-07-02 19:09:21 -0300

    testing github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(
   {
     plugins: [react()],
-    resolve: { alias: { 'src': '/src' } }
+    resolve: { alias: { 'src': '/src' } },
+    base: '/review-forge/'
   })
