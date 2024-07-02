dca998f0ea1b09a2114c9f0f180dc43ae16da4bd
jdickman <zenador15@gmail.com>
2024-07-02 15:21:42 -0400

    update api to accept post requests

--- src/api/BaseApi.ts
+++ src/api/BaseApi.ts
@@ -2,13 +2,27 @@ import axios from 'axios'
 
 export const BaseApi = {
   get,
+  post,
 } as const
 
-export const serverRoute = `http://localhost:5678` as const
+// export const localServerRoute = `http://localhost:5678` as const
+export const serverRoute = `http://34.138.3.116:8080` as const
 
 async function get<T>(endpoint: string, query: string = ''): Promise<T> {
-  const url = endpoint + (query ? `${encodeURIComponent(query)}` : '')
+  const url = endpoint + (query ? `?${query}` : '')
   const response = await axios.get(`${serverRoute}/${url}`)
+
+  if (response.status === 200) {
+    return response.data
+  } else {
+    throw new Error('Failed to fetch: ' + endpoint)
+  }
+}
+
+async function post<T>(endpoint: string, query: string = '', body = {}): Promise<T> {
+  const url = endpoint + (query ? `?${query}` : '')
+  const response = await axios.post(`${serverRoute}/${url}`, body)
+
   if (response.status === 200) {
     return response.data
   } else {
